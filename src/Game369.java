a749afa4fd53501d743230dd6b593d89422863e2
NayounSeo <parpe94@naver.com>
2016-03-02 20:38:46 +0900

    연습문제 03

--- src/Game369.java
+++ src/Game369.java
@@ -6,6 +6,7 @@ public class Game369 {
 		//사실 이 rem1~은 배열로 구현하는 것이 더 좋을 텐데 toInt 자릿수만큼 사이즈로
 		//String toString = String.valueOf(toInt);
 		for (int i=1; i<100; i++) {
+			//초기화는 for loop 안에서 하자구요 님아.....
 			int rem1 = i%10;
 			int rem2 = (i - rem1)/10;
 			int count = 0;
