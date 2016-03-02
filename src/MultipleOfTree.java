04b54f134ea074cf5bafc6abae7559a3ba869a11
NayounSeo <parpe94@naver.com>
2016-03-02 19:46:53 +0900

    연습문제 01

---
+++ src/MultipleOfTree.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MultipleOfTree {
+	public static void main (String[] args) {
+		System.out.println("수를 입력하시오 : ");
+		Scanner scanner = new Scanner(System.in);
+		int input;
+		input = scanner.nextInt();
+		isThree(input);
+	}
+	
+	public static void isThree(int input) {
+		//왜 static으로 바꿔야 하는지는 모르겠다.
+		if (input%3 == 0) {
+			System.out.println("3의 배수입니다");
+		}else {
+			System.out.println("3의 배수가 아닙니다.");
+		}
+	}
+}
